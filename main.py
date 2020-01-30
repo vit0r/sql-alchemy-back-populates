53b73dba4dd4c497e14a61280bd8db4d6e93fae3
vit0r <vitornascimentoaraujo@gmail.com>
2020-01-30 13:40:38 -0300

    fill databse

--- main.py
+++ main.py
@@ -15,12 +15,11 @@ Base.query = db_session.query_property()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
-    init_db()
-
 
 class Parent(Base):
     __tablename__ = "parent"
     id = Column(sqlite.INTEGER, primary_key=True)
+    name = Column(sqlite.TEXT)
     child = relationship("Child", uselist=False, back_populates="parent")
 
     def __repr__(self):
@@ -29,6 +28,7 @@ class Parent(Base):
 class Child(Base):
     __tablename__ = "child"
     id = Column(sqlite.INTEGER, primary_key=True)
+    name = Column(sqlite.TEXT)
     parent_id = Column(sqlite.INTEGER, ForeignKey("parent.id"))
     parent = relationship("Parent", back_populates="child")
 
@@ -40,3 +40,14 @@ if Path("database.db").exists():
     print(r)
 else:
     init_db()
+    try:
+        for r in range(1,101):
+            p = Parent(name=f"Parents{r}")
+            c = Child(name=f"Child{r}", parent_id=r)
+            db_session.add(p)
+            db_session.add(c)
+        db_session.commit()
+    except:
+        db_session.rollback()
+    finally:
+        print('databse filled')
